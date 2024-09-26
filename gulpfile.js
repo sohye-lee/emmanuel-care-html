09195b3bf0b05d413d58f1ca2372e4c02de1a4e0
sohye <sohyek@white64.com>
2024-09-26 09:09:02 -0400

    subpage gallery

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,5 @@
-// JS Files to Combine
-
-// const concatSharedJS = ['src/js/theme.js', 'src/js/theme/**/*.js'];
-// const concatEditorJS = ['src/js/editor.js', 'src/js/editor/**/*.js'];
-// const concatWPAdminJS = ['src/js/wp-admin.js', 'src/js/wp-admin/**/*.js'];
-
-
-// const dontConcatTheseJS = ['src/js/editor.js'];
-
-
-// Gulp dependencies
+ 
+ 
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const concat = require('gulp-concat');
@@ -16,13 +7,12 @@ const cleanCSS = require('gulp-clean-css');
 const uglify = require('gulp-uglify');
 const autoprefixer = require('gulp-autoprefixer');
 
-
-// Get current version of Build Pipeline
+ 
 const pkg = require('./package.json');
 const build_pipeline_version = pkg.version;
 
 
-// Add master scss files here
+// Master scss files here
 function scss() {
    return gulp.src(['source/_*.scss', 'source/*.scss', '!source/_*.scss'])
       .pipe(sass().on('error', sass.logError))
@@ -33,53 +23,11 @@ function scss() {
       .pipe(cleanCSS())
       .pipe(gulp.dest('build'));
 }
+ 
 
-
-// Combine and uglify non-excluded JS files
-function js_concat() {
-   return gulp.src(concatSharedJS)
-      .pipe(concat('theme.js'))
-      .pipe(uglify())
-      .pipe(gulp.dest('assets/js'));
-}
-
-
-// Combine and uglify non-excluded JS files (EDITOR ONLY) 
-function js_concat_editor() {
-   return gulp.src(concatEditorJS)
-     .pipe(concat('editor.js'))
-     .pipe(uglify())
-     .pipe(gulp.dest('assets/js'));
-}
-
-
-// Combine and uglify non-excluded JS files (WP-ADMIN ONLY) 
-function js_concat_wp_admin() {
-   return gulp.src(concatWPAdminJS)
-     .pipe(concat('wp-admin.js'))
-     .pipe(uglify())
-     .pipe(gulp.dest('assets/js'));
-}
-
-
-// Uglify, but do not concat, below 
-// function js_separated() {
-//    return gulp.src(dontConcatTheseJS)
-//      .pipe(uglify())
-//      .pipe(gulp.dest('assets/js'));
-// }
-
-
-// This will make you code faster
 function displayMessage() {
    console.log('\n\nBuild Pipeline v' + build_pipeline_version);
-   console.log(`
-██╗░░██╗░█████╗░██████╗░██████╗░██╗░░░██╗  ██████╗░███████╗██╗░░░██╗
-██║░░██║██╔══██╗██╔══██╗██╔══██╗╚██╗░██╔╝  ██╔══██╗██╔════╝██║░░░██║
-███████║███████║██████╔╝██████╔╝░╚████╔╝░  ██║░░██║█████╗░░╚██╗░██╔╝
-██╔══██║██╔══██║██╔═══╝░██╔═══╝░░░╚██╔╝░░  ██║░░██║██╔══╝░░░╚████╔╝░
-██║░░██║██║░░██║██║░░░░░██║░░░░░░░░██║░░░  ██████╔╝███████╗░░╚██╔╝░░
-╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░░░░░░░╚═╝░░░  ╚═════╝░╚══════╝░░░╚═╝░░░
+   console.log(`################################################
 `);
 }
 
@@ -88,11 +36,7 @@ function displayMessage() {
 function watch() {
    displayMessage();
    gulp.watch('source/**/*.scss', scss);
-   // gulp.watch(concatSharedJS, js_concat);
-   // gulp.watch(concatEditorJS, js_concat_editor);
-   // gulp.watch(concatWPAdminJS, js_concat_wp_admin);
  }
 
-
-// Default action if "gulp" ran with no parameters
+ 
 exports.default = watch;
